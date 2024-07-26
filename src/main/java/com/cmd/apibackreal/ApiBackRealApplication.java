46cc38f83f2d10c8fd2844f027c6fc64d18cf383
cmd <3417372950@qq.com>
2024-07-26 22:41:57 +0800

    init

---
+++ src/main/java/com/cmd/apibackreal/ApiBackRealApplication.java
@@ -0,0 +1,13 @@
+package com.cmd.apibackreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBackRealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBackRealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmd/apibackreal/ApiBackRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmd.apibackreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackRealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
