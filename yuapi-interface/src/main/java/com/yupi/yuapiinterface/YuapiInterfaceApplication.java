13ad01fb26ca4665ef56580e36f1d112c9923958
cmd <3417372950@qq.com>
2024-08-10 11:06:57 +0800

    2024-08-10

--- yuapi-interface/src/main/java/com/yupi/yuapiinterface/YuapiInterfaceApplication.java
+++ yuapi-interface/src/main/java/com/yupi/yuapiinterface/YuapiInterfaceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * YuApi 模拟接口入口类
+ * Api 模拟接口入口类
  */
 @SpringBootApplication
 @EnableDubbo
